9244ce529512377842f53df55dd21d271251c934
HasaMahawatta <92071475+HasaMahawatta@users.noreply.github.com>
2022-09-20 09:37:54 +1000

    show results

--- python.py
+++ python.py
@@ -1,1 +1,19 @@
 import random
+
+Choices =["Rock","Paper","Scissors"]
+
+While True
+Print ("Make your throw")
+user_choice =input("  Type Rock, Paper or Scissors: ")
+if(user_choice in Choices):
+  computer_choice =random.choice(Choices)
+  Print(f"\nYou threw '{user_choice}',the computer threw '{computer_choice}'") 
+else:
+  Print(f"\nYou typed '{user_choice}' which isn't a valid throw.")
+
+again = input("\nWant some more? (y/n):")
+if again.lower() =="n":
+  break
+  Print()
+
+Print("\nGood Bye")
